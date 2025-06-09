96fe7659cfb05107a39521bec28e7ef811c54a8f
Suresh-75 <suresh710k@gmail.com>
2025-06-09 10:42:23 +0530

    api changes

--- Backend/server.js
+++ Backend/server.js
@@ -8,28 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
-
-app.use("/api", require("./routes/riskRoutes"));
-=======
-// mongoose.connect(process.env.MONGODB_URI)
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error(err));
-
-mongoose.connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000 // 5 seconds timeout instead of 10s
+    serverSelectionTimeoutMS: 5000,
   })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
-  
-app.use('/api', require('./routes/riskRoutes'));
->>>>>>> 73affc3e94702b9458dfed8b17461b717c79c46d
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use("/api", require("./routes/riskRoutes"));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
